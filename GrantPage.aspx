c3496c2843e5a3f801e47f51835ac6f3904a6dcb
udomugo <jmorren.contact@gmail.com>
2017-03-14 12:00:22 -0700

    ITC172_Assignment6
    Initial ITC 172 Assignment 6 Commit

---
+++ GrantPage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GrantPage.aspx.cs" Inherits="GrantPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Apply for a Grant</h1>
+        <asp:DropDownList ID="GrantsDropDownList" runat="server"></asp:DropDownList>
+
+        <p>Reason for Grant Request <asp:TextBox ID="ExplainTextBox" runat="server"></asp:TextBox></p>
+        <p>Enter the Amount $ <asp:TextBox ID="AmountTextBox" runat="server"></asp:TextBox></p>
+        <p><asp:Button ID="GrantButton" runat="server" Text="Submit" OnClick="GrantButton_Click" />
+        <asp:Label ID="ResultLabel" runat="server" Text=""></asp:Label></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Register.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CAServiceReference;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RegisterButton_Click(object sender, EventArgs e)
+    {
+        CAServiceClient cas = new CAServiceClient();
+        PersonLite pl = new PersonLite();
+        pl.LastName = LastNameTextBox.Text;
+        pl.FirstName = FirstNameTextBox.Text;
+        pl.Email = EmailTextBox.Text;
+        pl.Apartment = AptTextBox.Text;
+        pl.Street = StreetTextBox.Text;
+        pl.City = CityTextBox.Text;
+        pl.State = StateTextBox.Text;
+        pl.HomePhone = HomePhoneTextBox.Text;
+        pl.WorkPhone = WorkPhoneTextBox.Text;
+        pl.Password = NewPassTextBox.Text;
+        bool result = cas.RegisterPerson(pl);
+        if (result)
+        {
+            ResultLabel.Text = "Registration Successful";
+        }
+        else
+        {
+            ResultLabel.Text = "Registration Failed";
+        }
+    }
+}
